5a6da1ef8b22ae57a5f8c9b9d3cab58647e11d11
scode <50059909+bcodxs@users.noreply.github.com>
2023-11-18 19:40:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     const textElement = document.getElementById('welcome-text');
-    const text = "¡Bienvenid@ web en construcción X(!";
+    const text = "Bienvenid@ web en construcción X(";
     text.split('').forEach((char, index) => {
         setTimeout(() => {
             textElement.innerHTML += char;
