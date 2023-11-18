44b6002f4bc2a08695d0803e45c6df976a7e3620
scode <50059909+bcodxs@users.noreply.github.com>
2023-11-18 19:29:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
     const textElement = document.getElementById('welcome-text');
-    const text = "¡Bienvenido, página en Construcción )X!";
-
+    const text = "¡Bienvenid@ web en construcción!";
     text.split('').forEach((char, index) => {
         setTimeout(() => {
             textElement.innerHTML += char;
-        }, 150 * index);
+        }, 100 * index);
     });
 });
